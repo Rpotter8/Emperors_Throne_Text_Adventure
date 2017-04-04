47ebd8dd06dbd7267af2ff173c4e15efeecc8fcd
spyguy31796 <planetalec@live.com>
2017-04-04 14:26:09 -0700

    More updates to allow the start of a new game
    Now has story and starting room

--- Core.py
+++ Core.py
@@ -37,7 +37,20 @@ print("""
          |                    |     |  |      |  |    |                   |
          |                    |     |  |      |  |    |                   |
          |                    |_____|__|______|__|____|                   |""")
-
+input("Press Enter to Continue...")
+print("This is your father's castle. Yet your adventure does not start in the throne room, or the dining room, or even your bedroom.")
+input("Press Enter to Continue...")
+print("You have awoken this morning locked in the jail cell in the cellar of the castle. Surrounded by rats and other undesirables.")
+input("Press Enter to Continue...")
+print("Your memory slowly returns as you clear the sleep from your eyes...")
+print("You remember the violent murder of your father at the hands of your uncle\nYou realize your uncle must have thrown you in the jail to keep you out of the way")
+print("until he can fully take over the kingdom!")
+print("Press Enter to Continue...")
+print("You quickly escape the jail cell and decide to confront your uncle in the throne room!")
+input("Press Enter to Continue...")
+#Game Starts Here!
+JailCell = Environment([Entity("Non-descript Potion","Potion",0,5)],"smelly dark room outside your jail cell.")
+print(JailCell.toString())
 #Testing RunCode
 
 #objects = []
