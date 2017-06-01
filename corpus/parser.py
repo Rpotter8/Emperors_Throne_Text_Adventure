3a5080358a01cabd021d08d2ffa8d0032238b6e3
Ryan Potter <rjp24@uw.edu>
2017-06-01 14:05:56 -0700

    Parser works... to a degree

--- corpus/parser.py
+++ corpus/parser.py
@@ -6,4 +6,16 @@ while(1):
     nb = input("Enter a sentence: ")
     nb = nltk.word_tokenize(nb)
     nb = nltk.pos_tag(nb)
-    print(str(nb))
+    print(nb)
+
+    verb = None
+    action = None
+    for i in nb:
+        if(i[1] == "VBP" or i[1] == "VBD"):
+            verb = i[0]
+        if(i[1] == "RB" or i[1] == "RP" or i[1] == "NNS" or
+            i[1] == "NN"):
+            action = i[0]
+        if(verb != None and action != None):
+            print("Verb: " + verb +", Action: " + action)
+            break
