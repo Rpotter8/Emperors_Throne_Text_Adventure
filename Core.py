19bcd7077d4e178037c58538b857535d50b707c7
ghs9 <ghs9@uw.edu>
2017-06-05 12:23:46 -0700

    equipment reballance

--- Core.py
+++ Core.py
@@ -84,15 +84,15 @@ print("""		.		.:			.                  :
 
 
 input("Press Enter to Continue...")
-print("This is your father's castle. Yet your adventure does not start in the throne room, or the dining room, or even your bedroom.")
+print("This is your father's castle. Yet your adventure does not start in the throne\nroom, or the dining room, or even your bedroom.")
 input("Press Enter to Continue...")
-print("You have awoken this morning locked in the jail cell in the cellar of the castle. Surrounded by rats and other undesirables.")
+print("You have awoken this morning locked in the jail cell in the cellar of the\ncastle. Surrounded by rats and other undesirables.")
 input("Press Enter to Continue...")
 print("Your memory slowly returns as you clear the sleep from your eyes...")
-print("You remember the violent murder of your father at the hands of your uncle\nYou realize your uncle must have thrown you in the jail to keep you out of the way")
-print("until he can fully take over the kingdom!")
+print("You remember the violent murder of your father at the hands of your uncle\nYou realize your uncle must have thrown you in the jail to keep you out of")
+print("the way until he can fully take over the kingdom!")
 input("Press Enter to Continue...")
-print("You quickly escape the jail cell and decide to confront your uncle in the throne room!")
+print("You quickly escape the jail cell and decide to confront your uncle in the\nthrone room!")
 input("Press Enter to Continue...")
 #Game Starts Here!
 #Predefined location, with one potion type random item.
